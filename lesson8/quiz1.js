e74d292760c630deeecffda144d9d02ac13aca9c
Melissa <meliarneau@gmail.com>
2018-04-01 18:30:35 +0100

    Create JS quiz Template Literals quiz 1

---
+++ lesson8/quiz1.js
@@ -0,0 +1,18 @@
+/* 
+Directions:
+
+By using template literals, you can drop the quotes along with the string concatenation operator. Also, you can reference the object's properties inside expressions.
+
+Here, you try. Change the greeting string below to use a template literal. Also, feel free to swap in your name for the placeholder.
+
+Instructions: Change the `greeting` string to use a template literal.
+
+
+const myName = '[NAME]';
+const greeting = 'Hello, my name is ' + myName;
+console.log(greeting);
+*/
+
+const myName = 'Melissa';
+const greeting = `Hello, my name is ${myName}`;
+console.log(greeting);
