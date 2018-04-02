76220a6b6fd01ec0864f949657657a5d8a72ad29
Melissa <meliarneau@gmail.com>
2018-04-02 14:39:45 +0100

    Create Spread JS quiz about the spread operator

---
+++ lesson8/spread.js
@@ -0,0 +1,17 @@
+/*
+Instructions: Use the spread operator to combine the `fruits` and `vegetables` arrays into the `produce` array.
+
+const fruits = ["apples", "bananas", "pears"];
+const vegetables = ["corn", "potatoes", "carrots"];
+
+const produce = [];
+
+console.log(produce);
+*/
+
+const fruits = ["apples", "bananas", "pears"];
+const vegetables = ["corn", "potatoes", "carrots"];
+
+const produce = [...fruits, ...vegetables];
+
+console.log(...produce);
